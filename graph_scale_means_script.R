f07fca47419f30fde622ce0c9fa46b93c473c61c
marios <msthoma@yahoo.com>
2020-07-17 16:28:38 +0300

    create bar chart with ggplot2

--- graph_scale_means_script.R
+++ graph_scale_means_script.R
@@ -3,4 +3,7 @@ library(googlesheets4)
 library(ggplot2)
 gs4_deauth()
 
-read_sheet(ss = sheet_id, sheet = worksheet_name, range = range)
+results <- read_sheet(ss = sheet_id, sheet = worksheet_name, range = range)
+
+bar_chart <- ggplot(results, aes(Scale)) + geom_bar(aes(weight = Mean))
+bar_chart
