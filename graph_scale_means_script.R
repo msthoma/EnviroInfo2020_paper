59db54894844c51cef574205fa2d0670dffad600
marios <msthoma@yahoo.com>
2020-07-17 20:30:47 +0300

    update script

--- graph_scale_means_script.R
+++ graph_scale_means_script.R
@@ -3,11 +3,33 @@ library(googlesheets4)
 library(ggplot2)
 gs4_deauth()
 
+
 results <- read_sheet(ss = sheet_id, sheet = worksheet_name, range = range)
 
-results$Category <- factor(results$Category, levels = c("Excellent", "Good", "Above Average", "Below Average", "Bad"))
+categories <- c("Excellent", "Good", "Above Average", "Below Average", "Bad")
+color_scale <- c("#00cc00", "#66ff66", "#92d050", "#ffc000", "#ff0000")
 
-bar_chart <- ggplot(results, aes(Scale)) + 
-  geom_bar(aes(weight = Mean, fill = Category)) +
-  scale_fill_discrete(drop=FALSE) + scale_x_discrete(drop=FALSE)
-bar_chart
+results$Category <- factor(results$Category, levels = categories)
+
+bar_chart2 <-
+  ggplot(results, aes(x = Scale, y = Mean, fill = Category)) +
+  geom_bar(stat = "identity") +
+  scale_fill_manual(name = "",
+                    values = color_scale,
+                    drop = FALSE) +
+  scale_x_discrete(drop = FALSE) +
+  scale_y_continuous(limits = c(-1, 3)) +
+  geom_hline(aes(yintercept = 0)) +
+  geom_hline(aes(yintercept = 1),  colour="grey", linetype="dashed") +
+  geom_hline(aes(yintercept = -1),  colour="grey", linetype="dashed") +
+  geom_text(aes(label=round(Mean, 2)), vjust=1.5) +
+  theme(axis.ticks.length.x = unit(0, "points"),
+        panel.background = element_rect(fill = "white"),
+        axis.line.y = element_line(color = "black"),
+        axis.title = element_text(size=11),
+        axis.text = element_text(size=11),
+        legend.position = c(0.9, 0.88),
+        axis.title.x = element_blank())
+
+bar_chart2
+ggsave("live_means_bar_chart.pdf", width = 18, height = 12, units = "cm")
