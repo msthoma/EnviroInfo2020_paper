f33f763e69cf4bf64ea9cb8f7277adbad7c7c402
marios <msthoma@yahoo.com>
2020-07-17 17:09:29 +0300

    show all categories in legend

--- graph_scale_means_script.R
+++ graph_scale_means_script.R
@@ -5,5 +5,9 @@ gs4_deauth()
 
 results <- read_sheet(ss = sheet_id, sheet = worksheet_name, range = range)
 
-bar_chart <- ggplot(results, aes(Scale)) + geom_bar(aes(weight = Mean))
+results$Category <- factor(results$Category, levels = c("Excellent", "Good", "Above Average", "Below Average", "Bad"))
+
+bar_chart <- ggplot(results, aes(Scale)) + 
+  geom_bar(aes(weight = Mean, fill = Category)) +
+  scale_fill_discrete(drop=FALSE) + scale_x_discrete(drop=FALSE)
 bar_chart
